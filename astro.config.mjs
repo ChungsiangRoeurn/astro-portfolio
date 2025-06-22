12000b7a7c9a4763d6f031c0f243ea373bbe9cde
Chungsiang <chungsiang@example.com>
2025-06-22 22:48:46 +0700

    Implement code changes to enhance functionality and improve performance

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
-import { defineConfig } from "astro/config";
-import react from "@astrojs/react";
-
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
 
 export default defineConfig({
-  integrations: [tailwind(), react(), image()],
-  devToolbar: {
-    enabled: false,
-  },
+  integrations: [
+    tailwind(),
+    react()
+  ],
+  image: {
+    service: {
+      entrypoint: 'astro/assets/services/sharp'
+    }
+  }
 });
