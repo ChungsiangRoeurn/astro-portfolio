8dc7352534ce135e898797b28de7611946dbbd53
Chungsiang Roeurn <mrchungsiang10@gmail.com>
2026-04-04 11:33:12 +0700

    fix: make SEO friendly

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
+  site: 'https://chungsiang.co',
   integrations: [
+    sitemap(),
     tailwind(),
     react()
   ],
